cae425889081dee35095d28927e62d23b37781d0
Clinton Bush <cbush06@gmail.com>
2018-11-07 20:54:52 -0500

    Applied named pipes for receiving interrupts from machine simulator

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 func main() {
 	var cpu cpu.CPU
 	cpu.Init()
+	cpu.EnableInterrupts = true
 
 	// Open Interrupt pipe
 	interruptPipe, err := fifo.New("intel8080_interrupt", os.O_CREATE|os.O_RDONLY|fifo.O_NONBLOCK, 0666)
 	if err != nil {
-		panic("Unable to connect to named pipe [intell8080_interrupt]")
+		panic("Unable to connect to named pipe [intell8080_interrupt]: " + err.Error())
 	}
 
 	defer interruptPipe.Close()
@@ -26,13 +27,13 @@ func main() {
 		if cpu.EnableInterrupts {
 			// Check for Interrupt; if set, execute interrupt instruction cycle
 			if readLen, err := interruptPipe.Read(interruptData); readLen == 1 && err == nil {
-				fmt.Println("INTERRUPT")
+				fmt.Printf("INTERRUPT 0x%2X\n", interruptData[0])
 				// cpu.InterruptInstructionCycle()
 				continue
 			}
 		}
 		fmt.Println("Standard Execution")
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 
 }
